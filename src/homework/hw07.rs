564482b226ecf59b6a12c25eaa09bc7b42588dec
Denis <135324093+jetx86@users.noreply.github.com>
2025-05-27 18:00:41 +0300

    Update hw07.rs

---
+++ src/homework/hw07.rs
@@ -0,0 +1,26 @@
+fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().collect::<String>()
+            } else if c.is_uppercase() {
+                c.to_lowercase().collect::<String>()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
+}
+
+#[test]
+fn test() {
+    let data = [
+        ("Hello", "hELLO"),
+        ("Привет", "пРИВЕТ"),
+    ];
+
+    data.iter().for_each(|(a, b)| {
+        assert_eq!(invert_the_case(a.to_string()), b.to_string());
+        assert_eq!(invert_the_case(b.to_string()), a.to_string());
+    });
+}
